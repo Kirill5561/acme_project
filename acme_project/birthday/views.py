3c838c9400a38ed20c00990cd51ec7a58fc3ec85
Kirill Sidorov <ksidorov2018@yandex.ru>
2023-10-15 21:57:40 +0400

    Add new file

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,21 +1,101 @@
-from django.shortcuts import render
+# from django.shortcuts import get_object_or_404, redirect, render
+# from django.core.paginator import Paginator
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView, DetailView)
+from django.urls import reverse_lazy
+
 
 from .forms import BirthdayForm
+from .models import Birthday
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
 
 
-def birthday(request):
-    form = BirthdayForm(request.GET or None)
-    # Создаём словарь контекста сразу после инициализации формы.
+class BirthdayMixin:
+    model = Birthday
+
+
+class BirthdayListView(BirthdayMixin, ListView):
+    ordering = 'id'
+    paginate_by = 10
+
+
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
+
+
+"""# Добавим опциональный параметр pk.
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(
+        request.POST or None,
+        # Файлы, переданные в запросе, указываются отдельно.
+        files=request.FILES or None,
+        instance=instance
+    )
     context = {'form': form}
-    # Если форма валидна...
+    # Сохраняем данные, полученные из формы, и отправляем ответ:
     if form.is_valid():
-        # ...вызовем функцию подсчёта дней:
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
-            # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
         )
-        # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+
+def delete_birthday(request, pk):
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        # ...и переадресовываем пользователя на страницу со списком записей.
+        return redirect('birthday:list')
+    # Если был получен GET-запрос — отображаем форму.
+    return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем список всех объектов с сортировкой по id.
+    birthdays = Birthday.objects.order_by('id')
+    # Создаём объект пагинатора с количеством 10 записей на страницу.
+    paginator = Paginator(birthdays, 5)
+
+    # Получаем из запроса значение параметра page.
+    page_number = request.GET.get('page')
+    # Получаем запрошенную страницу пагинатора.
+    # Если параметра page нет в запросе или его значение не приводится к числу,
+    # вернётся первая страница.
+    page_obj = paginator.get_page(page_number)
+    # Вместо полного списка объектов передаём в контекст
+    # объект страницы пагинатора
+    context = {'page_obj': page_obj}
+    return render(request, 'birthday/birthday_list.html', context)"""
